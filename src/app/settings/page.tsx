fdb1fc127f4c59b1abbf9458ca2c872b3398d15e
hqa-dev <hawkara@icloud.com>
2026-03-05 15:29:08 +0700

    settings: rename notifications label to پەیامەکان

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -96,7 +96,7 @@ export default function SettingsPage() {
                 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke={T.textDim} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
                   <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" /><path d="M13.73 21a2 2 0 0 1-3.46 0" />
                 </svg>
-                <span style={{ fontSize: 14, color: T.text }}>ئاگادارییەکانی پوش</span>
+                <span style={{ fontSize: 14, color: T.text }}>پەیامەکان</span>
               </div>
               <Toggle />
             </div>
